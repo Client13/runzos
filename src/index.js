fc4365118e9ae2ca9734508bedd210e0e2f9e3e7
Client13 <76941677+Client13@users.noreply.github.com>
2022-06-15 21:09:27 +0530

    Hehe

--- src/index.js
+++ src/index.js
@@ -20,7 +20,126 @@ Sentry.init({
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-  <Suspense fallback={<h1>Booting Up. Runz Os. c 2022</h1>}>
+  <Suspense fallback={
+<style>
+html {
+  height: 100%;
+}
+
+body,
+html {
+   height: 100vh;
+   
+   font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+}
+
+.backgroundImage {
+   background-image: url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSONcgPeMWZtUQFX46s7zVeRnjztnucUtYLKLsiq3J3qBkKAXWFQLrTxIA&s=10");
+   filter: blur(8px);
+   height: 100%;
+   background-position: center;
+   background-repeat: no-repeat;
+   background-size: cover;
+}
+.loader {
+  position: absolute;
+  top: calc(50% - 32px);
+  left: calc(50% - 32px);
+  width: 64px;
+  height: 64px;
+  border-radius: 50%;
+  perspective: 800px;
+}
+.row {
+  position: absolute;
+  top: calc(50% - -20px);
+  left: calc(50% - 32px);
+  width: 64px;
+  height: 64px;
+  border-radius: 50%;
+  perspective: 800px;
+  }
+.inner {
+  position: absolute;
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;  
+}
+
+.inner.one {
+  left: 0%;
+  top: 0%;
+  animation: rotate-one 1s linear infinite;
+  border-bottom: 3px solid #EFEFFA;
+}
+
+.inner.two {
+  right: 0%;
+  top: 0%;
+  animation: rotate-two 1s linear infinite;
+  border-right: 3px solid #EFEFFA;
+}
+
+.inner.three {
+  right: 0%;
+  bottom: 0%;
+  animation: rotate-three 1s linear infinite;
+  border-top: 3px solid #EFEFFA;
+}
+
+@keyframes rotate-one {
+  0% {
+    transform: rotateX(35deg) rotateY(-45deg) rotateZ(0deg);
+  }
+  100% {
+    transform: rotateX(35deg) rotateY(-45deg) rotateZ(360deg);
+  }
+}
+
+@keyframes rotate-two {
+  0% {
+    transform: rotateX(50deg) rotateY(10deg) rotateZ(0deg);
+  }
+  100% {
+    transform: rotateX(50deg) rotateY(10deg) rotateZ(360deg);
+  }
+}
+
+@keyframes rotate-three {
+  0% {
+    transform: rotateX(35deg) rotateY(55deg) rotateZ(0deg);
+  }
+  100% {
+    transform: rotateX(35deg) rotateY(55deg) rotateZ(360deg);
+  }
+}
+</style>
+
+
+
+
+
+<div class="backgroundImage"></div>
+
+<div class="loader">
+  
+  <div class="inner one"></div>
+  <div class="inner two"></div>
+  <div class="inner three"></div>
+  
+  
+  
+</div>
+
+<div class="row">
+     <p style="color:white;text-align : center"><strong>Initializing....</strong></p> 
+  </div>
+
+
+
+
+}>
     <Provider store={store}>
       <App />
     </Provider>
